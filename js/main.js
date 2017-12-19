9dda010df569ed86390b584c39713d06c1edd0e7
jparasha <jayant.parashar@capgemini.com>
2017-12-19 15:43:57 +0530

    url added

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 /*localstorage*/
 var availableStorage = true;
-var url = 'http://10.76.17.153:8000';
-//var url = 'https://coorg.herokuapp.com';
+//var url = 'http://10.76.17.153:8000';
+var url = 'https://coorg.herokuapp.com';
 var user = "there";
 var expenseType;
 if (typeof (Storage) !== "undefined") {
